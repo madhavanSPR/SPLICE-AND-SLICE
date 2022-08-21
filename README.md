cb7a6ab4fbb0ddd350a7b3c6d4aa70f15f96e154
madhavanSPR <mathuramu.3@gmail.com>
2022-08-21 23:51:15 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-r7mso9
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-r7mso9)
---
+++ index.js
@@ -0,0 +1,10 @@
+var array1 = [1, 2, 3, 4, 5];
+console.log(array1.splice(0, 3));
+console.log(array1);
+
+var array2 = [1, 2, 3, 4, 5];
+console.log(array2.slice(0, 2));
+console.log(array2);
+// COPY PASTE
+let example = array2.slice();
+console.log(example);
